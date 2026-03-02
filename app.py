5cefcf4520fa3cf535aa8a24899fb1f60c02cc64
dkoldyaev <dkoldyaev@gmail.com>
2026-03-02 20:28:42 +0100

    app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+FILE_PATH = "/data/secret.txt"
+
+@app.route('/')
+def get_val():
+    if os.path.exists(FILE_PATH):
+        with open(FILE_PATH, 'r') as f:
+            content = f.read().strip()
+        return f"File value: {content}"
+    return "No file!", 404
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
