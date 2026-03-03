40fef338de83fe1f60732d67177f122405be5794
dkoldyaev <dkoldyaev@gmail.com>
2026-03-03 23:28:23 +0100

    Refactor index function to get_val for file reading
    Replace index function with get_val to read from a file.

--- app.py
+++ app.py
@@ -3,17 +3,15 @@ import os
 
 app = Flask(__name__)
 
+FILE_PATH = "/files/secret.txt"
+
 @app.route('/')
-def index():
-    # Мы ищем переменную MY_APP_SECRET (или TEST, смотря как назовешь в Dokploy)
-    # В коде лучше использовать понятное имя, а в Dokploy связать его с секретом
-    secret_value = os.environ.get('SECRET_TEST', 'Секрет не найден в Environment')
-    
-    return f"""
-    <h1>Проверка секретов</h1>
-    <p>Значение секрета: <b>{secret_value}</b></p>
-    """
+def get_val():
+    if os.path.exists(FILE_PATH):
+        with open(FILE_PATH, 'r') as f:
+            content = f.read().strip()
+        return f"File value: {content}"
+    return "No file!", 404
 
 if __name__ == "__main__":
-    # Dokploy обычно ожидает порт 3000 или 5000, укажем явно
     app.run(host='0.0.0.0', port=5000)
